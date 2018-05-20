469eb4489f69bda5629ff8e56bcf437d351bb7a8
Takuya KIHARA <gray@tacck.net>
2018-05-20 18:23:12 +0900

    :sparkles: sec02-05

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -19,7 +19,7 @@
     <h2>Tasks List</h2>
     <ul>
         @foreach ($tasks as $task)
-            <li>{{ $task->title }} <input type="checkbox"
+            <li><a href="/tasks/{{ $task->id }}">{{ $task->title }}</a> <input type="checkbox"
                                           name="checkbox_{{ $task->id }}" {!! $task->executed ? 'checked="checked"' : '' !!}>
             </li>
         @endforeach
