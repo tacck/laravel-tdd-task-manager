415e9532e41baf1944040c1e39d9ef400f3abbbf
Takuya KIHARA <gray@tacck.net>
2018-05-27 19:18:22 +0900

    :sparkles: sec03-02

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -3,12 +3,15 @@
 namespace Tests\Unit;
 
 use App\Task;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * Get Seeder Tasks Test
      *
@@ -42,13 +45,32 @@ class TaskTest extends TestCase
         $this->assertTrue(boolval($task1->executed));
     }
 
-    public function testGetTaskDetail() {
+    public function testGetTaskDetail()
+    {
         $tasks = Task::find(2);
         $this->assertEquals('テストタスク', $tasks->title);
     }
 
-    public function testGetTaskDetailNotExists() {
+    public function testGetTaskDetailNotExists()
+    {
         $tasks = Task::find(0);
         $this->assertNull($tasks);
+    }
+
+    public function testUpdateTask()
+    {
+        $task = Task::create([
+            'title' => 'test',
+            'executed' => false,
+        ]);
+
+        $this->assertEquals('test', $task->title);
+        $this->assertFalse($task->executed);
+
+        $task->fill(['title' =>'テスト']);
+        $task->save();
+
+        $task2 = Task::find($task->id);
+        $this->assertEquals('テスト', $task2->title);
     }
 }
