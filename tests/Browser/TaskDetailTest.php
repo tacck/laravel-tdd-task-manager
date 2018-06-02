952edb0ccb148397db5e2a6ef75b7441fd8edde5
Takuya KIHARA <gray@tacck.net>
2018-06-02 22:53:03 +0900

    :sparkles: sec03-05

--- tests/Browser/TaskDetailTest.php
+++ tests/Browser/TaskDetailTest.php
@@ -2,23 +2,57 @@
 
 namespace Tests\Browser;
 
+use App\Task;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class TaskDetailTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
+    private $task;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->task = Task::create([
+            'title' => 'テストタスク',
+            'executed' => false,
+        ]);
+    }
+
     /**
      * Task Detail Test.
      *
      * @throws \Throwable
      */
-    public function testExample()
+    public function testShowDetail()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/tasks/2')
-                ->assertSee('テストタスク')
+            $browser->visit('/tasks/' . $this->task->id)
+                ->assertInputValue('#title', 'テストタスク')
                 ->screenshot("task_detail");
+        });
+    }
+
+    /**
+     * Task Post Test.
+     *
+     * @throws \Throwable
+     */
+    public function testPost()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/tasks/' . $this->task->id)
+                ->assertInputValue('#title', 'テストタスク')
+                ->type('#title', 'test task')
+                ->screenshot('task_post_typed')
+                ->press('更新')
+                ->pause(1000)
+                ->assertPathIs('/tasks/' . $this->task->id)
+                ->assertInputValue('#title', 'test task')
+                ->screenshot('task_post_pressed');
         });
     }
 }
--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -3,53 +3,13 @@
 namespace Tests\Unit;
 
 use App\Task;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
-    use DatabaseTransactions;
-
-    /**
-     * Get Seeder Tasks Test
-     *
-     * @return void
-     */
-    public function testGetSeederTasks()
-    {
-        // 全件取得
-        $tasks = Task::all();
-        // 3件取得できるはず
-        $this->assertEquals(3, count($tasks));
-
-        // 実行完了していないものを取得
-        $taskNotFinished = Task::where('executed', false)->get();
-        // 2件取得できるはず
-        $this->assertEquals(2, count($taskNotFinished));
-
-        // 実行完了しているものを取得
-        $taskFinished = Task::where('executed', true)->get();
-        // 1件取得できるはず
-        $this->assertEquals(1, count($taskFinished));
-
-        // 「テストタスク」というタイトルのレコードを取得
-        $task1 = Task::where('title', "テストタスク")->first();
-        // 実行完了していないはず
-        $this->assertFalse(boolval($task1->executed));
-
-        // 「終了タスク」というタイトルのレコードを取得
-        $task1 = Task::where('title', "終了タスク")->first();
-        // 実行完了しているはず
-        $this->assertTrue(boolval($task1->executed));
-    }
-
-    public function testGetTaskDetail()
-    {
-        $tasks = Task::find(2);
-        $this->assertEquals('テストタスク', $tasks->title);
-    }
+    use RefreshDatabase;
 
     public function testGetTaskDetailNotExists()
     {
@@ -67,7 +27,7 @@ class TaskTest extends TestCase
         $this->assertEquals('test', $task->title);
         $this->assertFalse($task->executed);
 
-        $task->fill(['title' =>'テスト']);
+        $task->fill(['title' => 'テスト']);
         $task->save();
 
         $task2 = Task::find($task->id);
